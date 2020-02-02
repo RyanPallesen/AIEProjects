fdea6d04714d78393cd8fad2e3befff745c62cb7
RyanPallesen <pallesenryan@gmail.com>
2020-02-03 10:05:13 +1100

    Added cards and cardhandler for object placement, added snapping to grid.

---
+++ SpareTimeProject/Assets/Scripts/Cards/CardHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHandler : MonoBehaviour
+{
+    public static CardHandler Instance;
+
+    private bool isPlacing;
+    public GameObject prefabToPlace;
+    public LayerMask layerMask;
+    private int StoredLayer;
+
+    public int gridSize;
+
+    public bool IsPlacing
+    {
+        get => isPlacing;
+        set
+        {
+            isPlacing = value;
+            if (value == true)
+            {
+                StoredLayer = prefabToPlace.layer;
+
+                prefabToPlace.layer = (int)Mathf.Log(layerMask.value, 2);
+
+                Debug.Log(prefabToPlace.layer);
+            }
+            else
+            {
+                prefabToPlace.layer = StoredLayer;
+
+                Debug.Log(prefabToPlace.layer);
+            };
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Only one CardHandler should exist at a given time");
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            IsPlacing = !IsPlacing;
+        }
+
+        if (IsPlacing)
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            {
+                prefabToPlace.transform.position = new Vector3(
+                Mathf.Round(hit.point.x / gridSize * gridSize),
+                Mathf.Round(hit.point.y / gridSize * gridSize),
+                Mathf.Round(hit.point.z / gridSize * gridSize)
+                                                              );
+            }
+
+            prefabToPlace.transform.Rotate(0, Input.mouseScrollDelta.y, 0);
+        }
+    }
+}
---
+++ SpareTimeProject/Assets/Scripts/Cards/CardObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CardObject : ScriptableObject
+{
+    public GameObject cardPrefabObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
