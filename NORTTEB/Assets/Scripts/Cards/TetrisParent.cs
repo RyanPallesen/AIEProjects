6612bc953cce838b19a3fd46e38650d7f9407660
ryan pallesen <pallesenryan@gmail.com>
2020-02-04 12:29:50 +1100

    Merged Tetranaut to AIEProjects

---
+++ NORTTEB/Assets/Scripts/Cards/TetrisParent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisParent : MonoBehaviour
+{
+    public bool isRepair = false;
+    public List<TetrisPiece.Square> squares = new List<TetrisPiece.Square>();
+}
---
+++ NORTTEB/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    
+    public void OnButtonQuit()
+    {
+       Debug.Log("Quit pressed");
+       Application.Quit();
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
