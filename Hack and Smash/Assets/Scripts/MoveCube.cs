47be7d68fb20f670dc252263e32a5a0ae1f64d6e
ryan pallesen <pallesenryan@gmail.com>
2020-03-23 00:33:37 +1100

    Update from home - Added hack and smash, deleted old projects

---
+++ Hack and Smash/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(
+            Random.RandomRange(-4, 4),
+            Random.RandomRange(-4, 4),
+            Random.RandomRange(-4, 4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
