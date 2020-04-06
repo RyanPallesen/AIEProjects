31f9f842c096c7f65ca483c47e3a393782c93178
ryan pallesen <pallesenryan@gmail.com>
2020-04-06 13:10:21 +1000

    Small home update

---
+++ Hack and Smash/Assets/Scripts/LocalManager.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkIdentity))]
+public class LocalManager : NetworkBehaviour
+{
+    public Player player;
+
+    public List<ScriptableObject> Primary;
+    public List<ScriptableObject> Secondary;
+    public List<ScriptableObject> Movement;
+    public List<ScriptableObject> Ultimate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hack and Smash/Assets/Scripts/SkillDef.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SkillDef", order = 1)]
+public class SkillDef : ScriptableObject
+{
+
+    public enum skillType
+    {
+        Primary,
+        Utility,
+        Ultimate,
+        COUNT
+    }
+
+    public MonoBehaviour monoBehaviour;
+    public string name;
+    public string description;
+    public Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
