e92a484e87077d032c142764c65f3bf6eb14e476
RyanPallesen <pallesenryan@gmail.com>
2020-06-29 22:13:50 +1000

    Hack and Smash major update

--- Hack and Smash/Assets/Scripts/LocalManager.cs
+++ Hack and Smash/Assets/Scripts/LocalManager.cs
@@ -3,25 +3,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent(typeof(NetworkIdentity))]
 public class LocalManager : NetworkBehaviour
 {
-    public Player player;
+    //Variables
+    public float speed = 6.0F;
+    public float mouseSpeed = 10.0F;
 
-    public List<ScriptableObject> Primary;
-    public List<ScriptableObject> Secondary;
-    public List<ScriptableObject> Movement;
-    public List<ScriptableObject> Ultimate;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public Camera myCamera;
+    public AudioListener myAudioListener;
 
-    // Start is called before the first frame update
-    void Start()
+    public new Rigidbody rigidbody;
+    private void Start()
     {
-        
-    }
+        if (!base.hasAuthority)
+        {
+            myCamera.enabled = false;
+            myAudioListener.enabled = false;
 
-    // Update is called once per frame
+        }
+        else//is local client
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+    
     void Update()
     {
-        
+        //if (!base.hasAuthority)
+        //{
+        //    myCamera.enabled = false;
+        //    myAudioListener.enabled = false;
+        //}
+
+        if (base.hasAuthority)
+        {
+
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * Time.deltaTime * speed;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position -= transform.right * Time.deltaTime * speed;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position -= transform.forward * Time.deltaTime * speed;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.right * Time.deltaTime * speed;
+            }
+
+            if(Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody.AddForce(transform.up * jumpSpeed);
+            }
+            transform.Rotate(new Vector3(0, Input.GetAxisRaw("Mouse X") * Time.deltaTime * mouseSpeed, 0));
+        }
     }
 }
